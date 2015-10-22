2baaca381faf872901a57b6c4f9bded79b21fe57
pabzdzdzwiagief <pabzdzdzwiagief@gmail.com>
2015-10-22 05:43:18 +0000

    Add a static file server for slides

---
+++ src/main/clojure/com/github/pabzdzdzwiagief/clojure/intro.clj
@@ -0,0 +1,6 @@
+(ns com.github.pabzdzdzwiagief.clojure.intro
+  (:import spark.Spark))
+
+(Spark/port 8080)
+(Spark/externalStaticFileLocation "src/main/resources/public")
+(Spark/get "/hello" (proxy [spark.Route] [] (handle [_ _] "Hello!")))
